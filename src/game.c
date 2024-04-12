d4972eb44d85c7be7c1d56288cc5253566389968
KevinPernu <pernu@kth.se>
2024-04-12 15:39:45 +0200

    Fixat basic meny

--- src/game.c
+++ src/game.c
@@ -22,6 +22,7 @@ struct ctx{
 
     Entity player;
     Entity background;
+    Entity menu;
     bool moving_left;
     bool moving_right;
     bool moving_up;
@@ -78,45 +79,68 @@ int main(int argc, char *argv[])
 
 
     int running = TRUE;
+    int menu = TRUE;
     SDL_Event event;
     const Uint8 *state;
-    while (running) {
-        state = SDL_GetKeyboardState(NULL);
-
-        ctx.moving_left = state[SDL_SCANCODE_A] > 0;
-        ctx.moving_right = state[SDL_SCANCODE_D] > 0;
-        ctx.moving_up = state[SDL_SCANCODE_W] > 0;
-        ctx.moving_down = state[SDL_SCANCODE_S] > 0;
-
+    
+    SDL_RenderClear(ctx.renderer);//clearar renderer
+    SDL_Surface *menuImage = IMG_Load("resources/mMenu.jpeg");//laddar in menu jpg
+    ctx.menu.tex = SDL_CreateTextureFromSurface(ctx.renderer, menuImage);
+    SDL_FreeSurface(menuImage);
+    SDL_RenderCopy(ctx.renderer, ctx.menu.tex, NULL, &ctx.background.dest);
+        SDL_RenderPresent(ctx.renderer); //presenterar menu bilden tills vidare
+    while(menu){
+        state = SDL_GetKeyboardState(NULL); //scannar efter input
+        if(state[SDL_SCANCODE_SPACE]>0){// om man trycker space hoppar den ur loopen, förstör bilden och går till running
+            menu =FALSE;
+            SDL_DestroyTexture(ctx.menu.tex);
+        }
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                running = FALSE;
+                if (event.type == SDL_QUIT) {
+                    menu = FALSE;
+                }
             }
-        }
-
-        if (ctx.moving_left && ctx.player.dest.x > 0) {
-            ctx.player.dest.x -= MOVE_SPEED;
-        }
-
-        if (ctx.moving_right && ctx.player.dest.x + ctx.player.dest.w < WINDOW_WIDTH) {
-            ctx.player.dest.x += MOVE_SPEED;
-        }
-
-        if (ctx.moving_up && ctx.player.dest.y > 0) {
-            ctx.player.dest.y -= MOVE_SPEED;
-        }
-
-        if (ctx.moving_down && ctx.player.dest.y + ctx.player.dest.h < WINDOW_HEIGHT) {
-            ctx.player.dest.y += MOVE_SPEED;
-        }
-
-
-        SDL_RenderClear(ctx.renderer);
-        SDL_RenderCopy(ctx.renderer, ctx.background.tex, NULL, &ctx.background.dest);
-        SDL_RenderCopy(ctx.renderer, ctx.player.tex, &ctx.player.source, &ctx.player.dest);
-
-        SDL_RenderPresent(ctx.renderer);
     }
+
+    
+        
+        while(running) {
+            state = SDL_GetKeyboardState(NULL);
+
+            ctx.moving_left = state[SDL_SCANCODE_A] > 0;
+            ctx.moving_right = state[SDL_SCANCODE_D] > 0;
+            ctx.moving_up = state[SDL_SCANCODE_W] > 0;
+            ctx.moving_down = state[SDL_SCANCODE_S] > 0;
+
+            while (SDL_PollEvent(&event)) {
+                if (event.type == SDL_QUIT) {
+                    running = FALSE;
+                }
+            }
+
+            if (ctx.moving_left && ctx.player.dest.x > 0) {
+                ctx.player.dest.x -= MOVE_SPEED;
+            }
+
+            if (ctx.moving_right && ctx.player.dest.x + ctx.player.dest.w < WINDOW_WIDTH) {
+                ctx.player.dest.x += MOVE_SPEED;
+            }
+
+            if (ctx.moving_up && ctx.player.dest.y > 0) {
+                ctx.player.dest.y -= MOVE_SPEED;
+            }
+
+            if (ctx.moving_down && ctx.player.dest.y + ctx.player.dest.h < WINDOW_HEIGHT) {
+                ctx.player.dest.y += MOVE_SPEED;
+            }
+
+
+            SDL_RenderClear(ctx.renderer);
+            SDL_RenderCopy(ctx.renderer, ctx.background.tex, NULL, &ctx.background.dest);
+            SDL_RenderCopy(ctx.renderer, ctx.player.tex, &ctx.player.source, &ctx.player.dest);
+
+            SDL_RenderPresent(ctx.renderer);
+        }
 
     // Release resources
     SDL_DestroyTexture(ctx.player.tex);
